fcb3c8531909af24382dff6b5f8bad95759914f1
soeun lee <thdmsdl87@gmail.com>
2022-08-20 15:16:00 +0900

    feat: Add AppDatabase
    수정사항
    1. DB singleton 객체를 위한 AppDatabase class add

--- app/src/main/java/com/example/zeroerror/data/persistence/AppDatabase.kt
+++ app/src/main/java/com/example/zeroerror/data/persistence/AppDatabase.kt
@@ -1,11 +1,10 @@
 package com.example.zeroerror.data.persistence
 
 import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
+import androidx.room.*
 import com.example.zeroerror.data.model.Inspect
 import com.example.zeroerror.data.model.Order
+import com.google.gson.Gson
 
 @Database(entities = [Order::class, Inspect::class], version = 1, exportSchema = true)
 abstract class AppDatabase : RoomDatabase() {
@@ -15,6 +14,7 @@ abstract class AppDatabase : RoomDatabase() {
 
     companion object {
         private var instance: AppDatabase? = null
+        private val gson = Gson()
 
         @Synchronized
         fun getInstance(context: Context): AppDatabase? {
@@ -24,10 +24,40 @@ abstract class AppDatabase : RoomDatabase() {
                         context.applicationContext,
                         AppDatabase::class.java,
                         "database-name"
-                    ).build()
+                    )
+                        .addTypeConverter(OrderListTypeConverter(gson))
+                        .addTypeConverter(OrderTypeConverter(gson))
+                        .build()
                 }
             }
             return instance
+        }
+    }
+    @ProvidedTypeConverter
+    class OrderListTypeConverter(private val gson: Gson) {
+
+        @TypeConverter
+        fun listToJson(value: List<Order>): String? {
+            return gson.toJson(value)
+        }
+
+        @TypeConverter
+        fun jsonToList(value: String): List<Order> {
+            return gson.fromJson(value, Array<Order>::class.java).toList()
+        }
+    }
+
+    @ProvidedTypeConverter
+    class OrderTypeConverter(private val gson: Gson) {
+
+        @TypeConverter
+        fun listToJson(value: Order): String? {
+            return gson.toJson(value)
+        }
+
+        @TypeConverter
+        fun jsonToList(value: String): Order {
+            return gson.fromJson(value, Order::class.java)
         }
     }
 }
