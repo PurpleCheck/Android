89db35a70db5edea29af30a7e23ff4f0bf751386
soeun lee <thdmsdl87@gmail.com>
2022-08-20 13:15:00 +0900

    feat: Add ListAdapter to RecyclerView
    수정사항
    1. ListAdapter를 RecyclerView에 add
    2. ViewModel의 productList를 submitList함
    3. ViewModel의 productList Observe

--- app/src/main/java/com/example/zeroerror/ui/CheckProduct/ProductListFragment.kt
+++ app/src/main/java/com/example/zeroerror/ui/CheckProduct/ProductListFragment.kt
@@ -1,6 +1,49 @@
 package com.example.zeroerror.ui.CheckProduct
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.zeroerror.databinding.FragmentProductlistBinding
 
-class ProductListFragment : Fragment(){
+class ProductListFragment: Fragment() {
+
+    private lateinit var binding: FragmentProductlistBinding
+    private lateinit var viewModel: CheckProductViewModel
+    private lateinit var adapter: ProductListAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        // 1. View Binding 설정
+        binding = FragmentProductlistBinding.inflate(inflater, container, false)
+
+        // 2. View Model 설정
+        viewModel = ViewModelProvider(requireActivity(), ViewModelProvider.NewInstanceFactory()) .get(
+            CheckProductViewModel::class.java)
+
+        // 3. Adapter 설정
+        adapter = ProductListAdapter()
+        adapter.setHasStableIds(true)
+        adapter.submitList(viewModel.productList.value?.toList())
+        binding.rvProductList.adapter = adapter
+        binding.rvProductList.layoutManager = LinearLayoutManager(activity)
+
+        return binding.root
+    }
+
+    // 4. View Model Item Observe
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.productList.observe(viewLifecycleOwner, Observer{
+            binding.rvProductList.post(Runnable { adapter.submitList(it.toList()) })
+        })
+    }
 }
