19589c433e256a6e2114258534112d9e49871f88
leesoeun98 <thdmsdl87@gmail.com>
2022-08-22 03:12:05 +0900

    feat: Add custom xml to ConfirmAlertDialog
    수정사항
    1. createDialog 쓰다가 onResponse내 intent안되는 버그 => 직접 xml 부여해서 Fragment lifecycle 내에서 intent 하는걸로 해결

--- app/src/main/java/com/example/zeroerror/ui/CheckTracking/ConfirmCheckAlertDialogFragment.kt
+++ app/src/main/java/com/example/zeroerror/ui/CheckTracking/ConfirmCheckAlertDialogFragment.kt
@@ -4,12 +4,16 @@ import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import com.example.zeroerror.R
 import com.example.zeroerror.data.model.FinishResponse
 import com.example.zeroerror.data.network.RetrofitService
+import com.example.zeroerror.databinding.FragmentConfirmcheckalertdialogBinding
 import com.example.zeroerror.ui.CheckInspect.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 import retrofit2.Call
@@ -19,21 +23,26 @@ import retrofit2.Response
 @AndroidEntryPoint
 class ConfirmCheckAlertDialogFragment: DialogFragment() {
 
+    private lateinit var binding: FragmentConfirmcheckalertdialogBinding
     private lateinit var inspectId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         inspectId = arguments?.getString("inspectId") ?: throw IllegalStateException("No args provided")
-        Log.d("INSPECT ID", inspectId)
     }
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
-        AlertDialog.Builder(requireContext())
-            .setMessage(getString(R.string.invoice_confirm))
-            .setPositiveButton(getString(R.string.invoice_confirm_ok)) { _, _ ->
-                patchInspectFinish(inspectId.toLong())
-            }
-            .create()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentConfirmcheckalertdialogBinding.inflate(inflater, container, false)
+        binding.btConfirmOk.setOnClickListener {
+            patchInspectFinish(inspectId = inspectId.toLong())
+        }
+        return binding.root
+    }
 
     private fun patchInspectFinish(inspectId: Long){
         val param = HashMap<String, Boolean>()
@@ -46,22 +55,23 @@ class ConfirmCheckAlertDialogFragment: DialogFragment() {
                             val body= response.body()
                             body?.let{
                                 if(body.finish){
-                                    val intent = Intent(activity?.applicationContext, MainActivity::class.java)
+                                    val intent = Intent(activity, MainActivity::class.java)
                                     intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                                     startActivity(intent)
                                     activity?.finish()
+                                    Log.d("FINISH?", "FINISH")
                                 }
                             }
                         }
                     } else { // response.code == 400 or 300
                         Toast.makeText(activity, "다시 시도해주세요", Toast.LENGTH_SHORT).show()
-                        Log.d("CLIENT_ERR", response.errorBody().toString())
+                        Log.e("CLIENT_ERR", response.errorBody().toString())
                     }
                 }
 
                 override fun onFailure(call: Call<FinishResponse>, t: Throwable) {
                     Toast.makeText(activity, "네트워크 연결상태를 확인해주세요", Toast.LENGTH_SHORT).show()
-                    Log.d("RETROFIT_ERR", t.message.toString())
+                    Log.e("RETROFIT_ERR", t.message.toString())
                 }
             }
         )
